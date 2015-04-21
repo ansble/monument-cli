aca5c1a8f32bd85fcd4373920141c4ab8960f963
Daniel Sellers <daniel@designfrontier.net>
2015-04-21 15:02:03 -0700

    begining work on #12 stubbed out all the things and now we can start writing the code that lays down the files

---
+++ generators/data/index.js
@@ -0,0 +1,96 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+
+module.exports = yeoman.generators.Base.extend({
+  initializing: function () {
+  },
+
+  prompting: function () {
+    var done = this.async();
+
+    // Have Yeoman greet the user.
+    this.log('Welcome to the ' + chalk.red('monumentjs cli') + '!');
+    this.log('So you want to create a new data handler... please stand by');
+
+    var prompts = [
+      {
+        type: 'confirm',
+        name: 'location',
+        message: 'Are you in the root of your project? (you need to be)',
+        default: true
+      }
+      , {
+        type: 'input',
+        name: 'dataName',
+        message: 'What is the name of this data handler?',
+        default: 'myDataHandler'
+      }
+      , {
+        type: 'input',
+        name: 'dataName',
+        message: 'What is the name of this data handler?',
+        default: 'myDataHandler'
+      }
+    ];
+
+    this.prompt(prompts, function (props) {
+      // this.location = props.location;
+
+      if(props.location){
+        this.pkg = this.fs.readJSON('./package.json');
+        this.routes = this.fs.readJSON('./routes.json');
+      }
+
+      done();
+    }.bind(this));
+  },
+
+  writing: {
+    app: function () {
+      var that = this
+        , routeReadHolder;
+
+      Object.keys(that.routes).forEach(function (route) {
+        var fileName = route.split('/')
+          , localRoute = route;
+
+        if(fileName[0] !== ''){
+          console.log(chalk.yellow('You have a route that doesn\'t begin with "/"... you should fix that'));
+          fileName = fileName[0];
+          localRoute = '/' + route;
+        } else {
+          fileName = fileName[1];
+        }
+
+        if(route === '/' || route.match(/^\/:/)){
+          fileName = 'main';
+        }
+
+        if(that.fs.exists(process.cwd() + '/routes/' + fileName + '.js')){
+          routeReadHolder = that.fs.read(process.cwd() + '/routes/' + fileName + '.js');
+          //check for all the verbs...
+          that.routes[route].forEach(function (verb) {
+            var regex = new RegExp('route:' + localRoute + ':' + verb.toLowerCase());
+
+            if(!routeReadHolder.match(regex)){
+              //the route does not yet exist... stub it out
+
+              routeReadHolder += '\r\n\r\n' + that.engine(that.fs.read(that.templatePath('_existingRoute.js')), {routePath: localRoute, routeVerb: verb});
+
+              that.fs.write(process.cwd() + '/routes/' + fileName + '.js', routeReadHolder);
+            }
+          });
+        } else {
+          routeReadHolder = that.engine(that.fs.read(that.templatePath('_route.js')), {});
+
+            that.routes[route].forEach(function (verb) {
+                routeReadHolder += '\r\n\r\n' + that.engine(that.fs.read(that.templatePath('_existingRoute.js')), {routePath: route, routeVerb: verb});
+            });
+
+            that.fs.write(process.cwd() + '/routes/' + fileName + '.js', routeReadHolder);
+        }
+      });
+    }
+  }
+});
