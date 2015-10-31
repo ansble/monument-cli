8677288b7430ccd469ac1ae96dd7939b3fde10d7
Daniel Sellers <daniel@designfrontier.net>
2015-10-30 23:48:22 -0600

    Removes the old YO generator files

--- generators/app/templates/_app.js
+++
@@ -1,8 +0,0 @@
-var monument = require('monument');
-
-monument.server({
-				routePath: './routes'
-				, templatePath: './templates'
-				, publicPath: './public'
-				, port: process.env.PORT || 3000
-			});
--- generators/routes/templates/_existingRoute.js
+++
@@ -1,5 +0,0 @@
-events.on('route:<%= routePath %>:<%= routeVerb %>', function (connection) {
-	'use strict';
-	
-	connection.res.end('route <%= routePath %> now responding to <%= routeVerb %> requests');
-});
