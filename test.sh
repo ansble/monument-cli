a2001df5505e45fda8999b1b9319065d6bbcb6cd
Daniel Sellers <daniel@designfrontier.net>
2016-10-17 08:40:43 -0600

    expanded tests

--- test.sh
+++ test.sh
@@ -6,3 +6,12 @@ npm link
 mkdir test-project && cd test-project
 cat ../test_stubs/project_answers | monument new .
 npm test
+
+#new route test
+cat ../test_stubs/routes_test.json > routes.json
+npm test
+
+#new data test
+cat ../test_stubs/data_obj_answers | monument data obj
+cat ../test_stubs/data_coll_answers | monument data coll
+npm test
