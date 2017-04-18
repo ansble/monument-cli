5196e86a2f436f4203d82de61de1686f3cefddce
Daniel Sellers <daniel@designfrontier.net>
2017-04-18 07:22:26 -0600

    corrects some spacing issues

--- templates/routes/existingRoute.js
+++ templates/routes/existingRoute.js
@@ -3,6 +3,6 @@
 module.exports = (vars) => {
 
   return `events.on('route:${vars.routePath}:${vars.routeVerb }', (connection) => {
-    connection.res.send('route ${vars.routePath } now responding to ${vars.routeVerb} requests');
+  connection.res.send('route ${vars.routePath } now responding to ${vars.routeVerb} requests');
 });`;
 };
--- templates/routes/newRoute.test.js
+++ templates/routes/newRoute.test.js
@@ -2,8 +2,8 @@
 module.exports = (options) => {
 
   return `    it('should respond to route:${options.routePath}:${options.routeVerb}', () => {
-        events.emit('route:${options.routePath}:${options.routeVerb}', fakeConnection);
+    events.emit('route:${options.routePath}:${options.routeVerb}', fakeConnection);
 
-        assert.strictEqual(fakeConnection.out().response, 'route ${options.routePath } now responding to ${options.routeVerb} requests');
-    });`;
+    assert.strictEqual(fakeConnection.out().response, 'route ${options.routePath } now responding to ${options.routeVerb} requests');
+  });`;
 };
