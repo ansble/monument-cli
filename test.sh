f178111def35c67632520d125403e9af9c986260
Daniel Sellers <dsellers@instructure.com>
2017-08-25 14:11:41 -0600

    Updates the connectionStub and other things
    ConnectionStub which is the backbone of route testing
    was broken by the update to harken in monument 3.0.11.
    This patch fixes it.
    
    It also bumps the version of monument being used, updates
    the default name to the name of the current version
    name of monument, updates the test.sh file with some
    logging...
    
    and that's pretty much it

--- test.sh
+++ test.sh
@@ -3,16 +3,19 @@ npm test
 npm link
 
 #project base tests
+echo "Testing the base project scaffold"
 mkdir test-project && cd test-project
 cat ../test_stubs/project_answers | monument new .
 npm test
 
 #new route test
+echo "Testing the new route project scaffold"
 cat ../test_stubs/routes_test.json > routes.json
 monument routes
 npm test
 
 #new data test
+echo "Testing the new data project scaffold"
 cat ../test_stubs/data_obj_answers | monument data obj
 cat ../test_stubs/data_coll_answers | monument data coll
 npm test
