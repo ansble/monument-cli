39c1f6c3c703bb89d4a39e17e5e09907cd5ee58f
Daniel Sellers <dsellers@instructure.com>
2018-02-28 21:11:06 -0700

    expanded tests to cover everything for both types of templates

--- test.sh
+++ test.sh
@@ -41,3 +41,21 @@ mkdir test-project && cd test-project
 cat ../test_stubs/project_answers_handlebars | monument new .
 npm link monument
 npm test
+
+#new route test
+echo ""
+echo "-------------------------------------------------------"
+echo "Testing the new route project scaffold with handlebars"
+echo "-------------------------------------------------------"
+cat ../test_stubs/routes_test.json > routes.json
+monument routes
+npm test
+
+#new data test
+echo ""
+echo "-------------------------------------------------------"
+echo "Testing the new data project scaffold with handlebars"
+echo "-------------------------------------------------------"
+cat ../test_stubs/data_obj_answers | monument data obj
+cat ../test_stubs/data_coll_answers | monument data coll
+npm test
